1044c9581a2368f4ab38ba771df0754eab2eef44
Reinald Reynoso <reireynoso@gmail.com>
2019-04-03 21:18:24 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,40 @@
+class Book
+    def initialize(title)
+      @title = title
+    end
+    
 
-
+    def title 
+      @title
+    end
+    
+    def author=(new_author)
+      @author = new_author
+    end
+    
+    def author
+      @author
+    end
+    
+    def page_count=(count)
+      @page_count = count
+    end
+    
+    def page_count
+      @page_count
+    end
+    
+    def genre=(type)
+      @genre = type
+    end
+    
+    def genre
+      @genre
+    end
+    
+    def turn_page
+      puts "Flipping the page...wow, you read fast!"
+    end
+    
+    
+end
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,53 @@
 # Make your shoe class here!
+
+class Shoe 
+  def initialize(brand)
+    @brand = brand
+  end
+  
+  def brand=(new_brand)
+    @brand = new_brand
+  end
+  
+  def brand
+    @brand
+  end
+  
+  def color=(col)
+    @color = col
+  end
+  
+  def color
+    @color
+  end
+  
+  def size=(size)
+    @shoe_size = size
+  end
+    
+  def size
+    @shoe_size
+  end
+  
+  def material=(type)
+    @material = type
+  end
+  
+  def material 
+    @material
+  end
+  
+  def condition=(cond)
+    @condition = cond
+  end
+  
+  def condition
+    @condition
+  end
+  
+  def cobble
+    puts "Your shoe is as good as new!"
+    @condition = "new"
+  end
+  
+end
